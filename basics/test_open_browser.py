5b96c267ab428d3599252832cee264a2c2d92c66
ankit <dubeysk2003@gmail.com>
2025-08-17 14:03:23 +0530

    Created folder and file structure for Selenium repo

--- basics/test_open_browser.py
+++ basics/test_open_browser.py
@@ -6,7 +6,10 @@ def test_open_google():
     driver = webdriver.Chrome()
     driver.get("https://www.google.co.in/")
     assert "Google" in driver.title
+    time.sleep(2)
     search_box = driver.find_element(By.NAME, "q")
+    time.sleep(2)
     search_box.send_keys("Selenium automation python")
     time.sleep(2)
     driver.quit()
+    
